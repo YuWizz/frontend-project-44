abf8fb4ce3821e2a832d35987ce240d377a069e8
JuliePro <prokofevajuliya@gmail.com>
2024-09-25 17:34:25 +0300

    fixed

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+export const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
-
-export default greeting;
